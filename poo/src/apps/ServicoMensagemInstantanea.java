fb6924857b72aed30ace4e9ee4bb3570af3f94fa
vfoliveiras <oliveirasvf@gmail.com>
2023-09-04 15:57:06 -0300

    Pilares POO

---
+++ poo/src/apps/ServicoMensagemInstantanea.java
@@ -0,0 +1,18 @@
+package apps;
+
+public abstract class ServicoMensagemInstantanea {
+	
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();
+	
+	//mais um método que todos os filhos deverão implementar
+	protected void salvarHistoricoMensagem() {
+		System.out.println("Histórico salvo!");
+	}
+	
+	
+	//somente os filhos conhecem este método
+	protected void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}	
+}
